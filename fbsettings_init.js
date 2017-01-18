76dac2d9720573ea7d83f528fdcbaa86a0dd19b3
MarkBrightFutureApps <marksim@UMICH.EDU>
2017-01-18 06:18:23 -0500

    v 0.3

--- fbsettings_init.js
+++ fbsettings_init.js
@@ -6,11 +6,11 @@
 */
 const Env = {
   firebase: {
-    apiKey: 'AIzaSyChfae1T55vJ_rFwfX_AMLlX_p30SZxRq4',
-    authDomain: 'simonsonchat.firebaseapp.com',
-    databaseURL: 'https://simonsonchat.firebaseio.com',
-    storageBucket: 'simonsonchat.appspot.com',
-    messagingSenderId: '283119063703'
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    storageBucket: '',
+    messagingSenderId: ''
   }
 }
 
